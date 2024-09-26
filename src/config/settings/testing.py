51b5adc082c2f7682a29ff2e08b4d77c398dfea4
apexdev <mwangi.em37@gmail.com>
2024-09-26 21:44:10 +0300

    chore(depr): Replace testing password hasher `sha1`
    Resolve deprecation warning raised by pytest for SHA1PasswordHasher
    
    [Warning]
    ```
    RemovedInDjango51Warning:
    django.contrib.auth.hashers.SHA1PasswordHasher is deprecated.
    ```
    
    [Note]
    - Replace with MD5 with the same goal to speed test runs.

--- src/config/settings/testing.py
+++ src/config/settings/testing.py
@@ -47,9 +47,8 @@ os.environ.setdefault("PYTEST_XDIST_AUTO_NUM_WORKERS", "auto")
 # NB: (MD5) cryptographically weak hashing algorithm unsuitable for production.
 # NB: (SHA1) less faster, but more secure than `MD5`; significantly faster than Django defaults.
 PASSWORD_HASHERS = [
-    "django.contrib.auth.hashers.SHA1PasswordHasher",
+    "django.contrib.auth.hashers.MD5PasswordHasher",
 ]
-
 
 # Exclude specific apps from the serialization process during test setup.
 # NB: Use with caution:
