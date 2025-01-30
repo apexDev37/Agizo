b96d68fe1cba2443be403c02715063c08d067068
apexdev <mwangi.em37@gmail.com>
2025-01-30 18:36:32 +0300

    chore(config): Add initial `gunicorn` config
    Define Gunicorn config to be used to run the process manager in prod.
    
    [Note]
    - Use gthread to better handle concurrent requests and idle threads.

---
+++ src/gunicorn.config.py
@@ -0,0 +1,20 @@
+"""Gunicorn config.
+
+Optimizes Gunicorn for handling Django requests efficiently.
+Leverages multiple workers and threads for concurrency, sets timeouts for
+graceful shutdowns, and logs output to stdout.
+"""
+
+import multiprocessing
+
+bind = "0.0.0.0:8000"
+workers = multiprocessing.cpu_count() * 2 + 1  # Recommended formula for workers
+threads = 2  # Handle blocking I/O with threads
+worker_class = "gthread"
+timeout = 30
+graceful_timeout = 30
+loglevel = "info"
+accesslog = "-"
+errorlog = "-"
+keepalive = 5
+preload_app = True
