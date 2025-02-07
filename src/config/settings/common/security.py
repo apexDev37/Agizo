08eaaf9ad6e7f84891ac0a9e6cd4da696fc5768d
apexdev <mwangi.em37@gmail.com>
2025-02-07 22:08:24 +0300

    chore(config): Add `prod` security layer
    Add dedicated module to host site security config.
    Apply security config to production env.
    
    [Commands]
    ➜ python3 src/manage.py check --deploy
    
    [Note]
    - Skip default security check for `SECURE_SSL_REDIRECT` (explain why)
    - Some security headers (CSP, CORS, CORP) not implemented
      These require 3rd-party packages to configure
      This is unnecessary overhead for the current context.
    
    [Docs]
    For details on Render's web service load-balancer/proxy
    See: https://community.render.com/t/proxy-https-settings/19249

---
+++ src/config/settings/common/security.py
@@ -0,0 +1,66 @@
+"""Security settings for the project.
+
+Centralize and manage all your security-related settings in this module.
+This provides granular control to configure in target or all environments.
+
+Thanks to blog by Adam Johnson!
+See: https://adamj.eu/tech/2019/04/10/how-to-score-a+-for-security-headers-on-your-django-website/
+
+For more details about this module
+See: https://docs.djangoproject.com/en/4.2/topics/security/
+"""
+
+# ================ #
+# X-XSS-Protection #
+# ================ #
+# Tell browser to block pages with detected XSS attacks.
+SECURE_BROWSER_XSS_FILTER = True
+
+# ========================= #
+# Strict-Transport-Security #
+# ========================= #
+# Tell browser to load your site over HTTPS only.
+# Note: Browsers will refuse to allow users to bypass insecure warnings to connect.
+# TODO(apexDev37): Update `SECURE_HSTS_SECONDS` to recommended minimum after testing nothing breaks.
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# ====================== #
+# X-Content-Type-Options #
+# ====================== #
+# Tell browser to opt out of MIME Sniffing (guessing response content-type).
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# =============== #
+# Referrer-Policy #
+# =============== #
+# Tell browser to send `Referer` only for requests to the same domain.
+SECURE_REFERRER_POLICY = "same-origin"
+
+# =============== #
+# X-Frame-Options #
+# =============== #
+# Tell browser to prevent site from being embedded into an <iframe>.
+X_FRAME_OPTIONS = "DENY"
+
+# ======= #
+# SSL/TLS #
+# ======= #
+# Reverse proxy sets header `X-Forwarded-Host` to upstream service.
+USE_X_FORWARDED_HOST = True
+
+# Tell Django to trust `X-Forwarded-Host` from TLS terminating reverse proxy.
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# ======== #
+# Sessions #
+# ======== #
+# Tell browser to ensure cookies are "secure" via transmission over HTTPS only.
+SESSION_COOKIE_SECURE = True
+
+# ==== #
+# CSRF #
+# ==== #
+# Tell browser to ensure CSRF-cookies are "secure" via transmission over HTTPS only.
+CSRF_COOKIE_SECURE = True
--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -16,6 +16,19 @@ from django.core.exceptions import ImproperlyConfigured
 
 from config.settings.common.base import *  # noqa: F403
 from config.settings.common.database import DATABASES
+from config.settings.common.security import (
+    CSRF_COOKIE_SECURE,
+    SECURE_BROWSER_XSS_FILTER,
+    SECURE_CONTENT_TYPE_NOSNIFF,
+    SECURE_HSTS_INCLUDE_SUBDOMAINS,
+    SECURE_HSTS_PRELOAD,
+    SECURE_HSTS_SECONDS,
+    SECURE_PROXY_SSL_HEADER,
+    SECURE_REFERRER_POLICY,
+    SESSION_COOKIE_SECURE,
+    USE_X_FORWARDED_HOST,
+    X_FRAME_OPTIONS,
+)
 from config.settings.environment.django import (
     ALLOWED_HOSTS,
     DEBUG,
@@ -42,3 +55,15 @@ DATABASES["default"] = dj_database_url.config(
     conn_max_age=500,
     conn_health_checks=True,
 )
+
+# Silenced checks not output to the console.
+# See: https://docs.djangoproject.com/en/4.2/ref/checks/
+SILENCED_SYSTEM_CHECKS = [
+    # W008 = `SECURE_SSL_REDIRECT = False`
+    #
+    # Reason:
+    #   Site runs behind a TLS-terminating reverse proxy that redirects all HTTP
+    #   requests to HTTPS. Given proxy forwarded requests to the site will be
+    #   insecure (non-HTTPS), this may cause infinite redirects.
+    "security.W008",
+]
